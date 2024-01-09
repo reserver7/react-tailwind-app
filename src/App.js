4990456d3f1b4d33ead143e4e55e1c9c4012fcc8
reserver7 <reserver7@gmail.com>
2024-01-09 15:42:59 +0900

    tailwind & postcss

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Support from "./components/Support";
+import Feature from "./components/Feature";
+import Feedback from "./components/Feedback";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import TopButton from "./components/TopButton";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Home />
+      <About />
+      <Support />
+      <Feature />
+      <Feedback />
+      <Contact />
+      <Footer />
+
+      <TopButton />
+    </>
   );
 }
 
---
+++ src/components/TopButton.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+export default function TopButton() {
+  const [showButton, setShowButton] = useState(false);
+
+  const scrollToTop = () => {
+    window.scroll({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+  useEffect(() => {
+    const handleShowButton = () => {
+      if (window.scrollY > 500) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    };
+
+    console.log(window.scrollY);
+    window.addEventListener("scroll", handleShowButton);
+    return () => {
+      window.removeEventListener("scroll", handleShowButton);
+    };
+  }, []);
+
+  return (
+    showButton && (
+      <div className="scroll__container">
+        <button id="top" onClick={scrollToTop} type="button">
+          {" "}
+          Top
+        </button>
+      </div>
+    )
+  );
+}
